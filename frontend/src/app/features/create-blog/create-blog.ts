ed9b225f368b91a2f34e1ea94d933bc5a74f21da
ismailsayen <ismailsvn02@gmail.com>
2026-01-12 21:55:37 +0100

    Enables blog post media uploads
    Allows users to upload media (images/videos) while creating a blog post.
    The uploaded media's URL is then inserted into the blog post content, enabling richer content creation.
    Also, the commit configures the backend database connection.

--- frontend/src/app/features/create-blog/create-blog.ts
+++ frontend/src/app/features/create-blog/create-blog.ts
@@ -1,9 +1,9 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { MarkdownModule } from 'ngx-markdown';
-import { JobsSelect } from "../auth/components/jobs-select/jobs-select";
+import { JobsSelect } from '../auth/components/jobs-select/jobs-select';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ValidJob } from '../auth/utils/customValidators';
-import { ButtonSubmit } from "../auth/components/button-submit/button-submit";
+import { ButtonSubmit } from '../auth/components/button-submit/button-submit';
 import { BlogService } from './services/blog.service';
 
 @Component({
@@ -12,75 +12,73 @@ import { BlogService } from './services/blog.service';
   templateUrl: './create-blog.html',
   styleUrl: './create-blog.scss',
 })
-
 export class CreateBlog {
-  blogService = inject(BlogService)
-  createForm = new FormGroup(
-    {
-      categorie: new FormControl('', {
-        validators: [ValidJob],
-        updateOn: 'submit',
-      }),
-      title: new FormControl('', {
-        validators: [
-          Validators.required,
-          Validators.minLength(10),
-          Validators.maxLength(50)
-        ]
-      }),
-      content: new FormControl('', {
-        validators: [
-          Validators.required,
-          Validators.minLength(100),
-          Validators.maxLength(10000)
-        ]
-      }),
-    }, {
-    updateOn: 'submit'
-  })
+  blogService = inject(BlogService);
+  data = signal<string | undefined | null>('');
+  createForm = new FormGroup({
+    categorie: new FormControl('', {
+      validators: [ValidJob],
+      updateOn: 'submit',
+    }),
+    title: new FormControl('', {
+      validators: [Validators.required, Validators.minLength(10), Validators.maxLength(50)],
+      updateOn: 'submit',
+    }),
+    content: new FormControl('', {
+      validators: [Validators.required, Validators.minLength(100), Validators.maxLength(10000)],
+    }),
+    media: new FormControl(null),
+  });
 
   get title() {
-    return this.createForm.get('title')
+    return this.createForm.get('title');
   }
 
   get content() {
-    return this.createForm.get('content')
+    return this.createForm.get('content');
   }
 
   get categorie() {
-    return this.createForm.get('categorie') as FormControl
+    return this.createForm.get('categorie') as FormControl;
+  }
+
+  get media() {
+    return this.createForm.get('media');
+  }
+
+  onUploadMedia(event: Event) {
+    const inputElement = event.target as HTMLInputElement;
+    if (!inputElement.files || inputElement.files.length === 0) return;
+    const file = inputElement.files[0];
+    const file_url = URL.createObjectURL(file);
+    this.content?.setValue(this.content?.value + `![Drag Racing](${file_url})`);
+    this.data.set(this.content?.value);
   }
 
   onSubmit() {
     if (this.createForm.invalid) {
-      this.createForm.markAllAsTouched()
-      return
+      this.createForm.markAllAsTouched();
+      return;
     }
     console.log(this.createForm.value);
 
     const body = this.createForm.getRawValue();
 
-    this.blogService.create(body).subscribe(
-      {
-        next: (res) => {
-          console.log(res);
-
-        },
-        error: (err) => {
-
-          if (err.status === 400 && err.error) {
-            Object.keys(err.error).forEach((field: any) => {
-              const control = this.createForm.get(field);
-              if (control) {
-                console.log(err);
-                control.setErrors({ backend: err.error[field] });
-              }
-            });
-          }
-
+    this.blogService.create(body).subscribe({
+      next: (res) => {
+        console.log(res);
+      },
+      error: (err) => {
+        if (err.status === 400 && err.error) {
+          Object.keys(err.error).forEach((field: any) => {
+            const control = this.createForm.get(field);
+            if (control) {
+              console.log(err);
+              control.setErrors({ backend: err.error[field] });
+            }
+          });
         }
-      }
-    )
+      },
+    });
   }
-
 }
