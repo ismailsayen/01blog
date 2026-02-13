af0363bb29a7e371fd2378443b48f078fd89f732
ismailsayen <ismailsvn02@gmail.com>
2026-02-13 18:18:53 +0100

    Optimizes blog and report queries for efficiency
    Refactors blog and report queries to select only necessary fields, improving performance and reducing data transfer overhead.
    
    This change also re-enables database connection settings in application.properties.

--- backend/src/main/java/com/blog/report/repositories/ReportRepository.java
+++ backend/src/main/java/com/blog/report/repositories/ReportRepository.java
@@ -1,7 +1,6 @@
 package com.blog.report.repositories;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -11,26 +10,21 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.data.util.Streamable;
 import org.springframework.stereotype.Repository;
 
-import com.blog.report.DTO.ReportDTO;
 import com.blog.report.DTO.ReportDTO.AllReports;
 import com.blog.report.models.ReportEntity;
 import com.blog.report.models.ReportTargetType;
 
 @Repository
 public interface ReportRepository extends JpaRepository<ReportEntity, Long> {
-    @Query(value = "SELECT DISTINCT p.*, u.user_name FROM reports p INNER JOIN users u ON p.user_id=u.id", nativeQuery = true)
-    public Page<AllReports> getNewRports(Pageable pageable);
-
-    @Query(value = "SELECT p.*, u.user_name  FROM reports p INNER JOIN users u ON p.user_id=u.id  where p.id=:id AND p.target_type=:target_type", nativeQuery = true)
-    public Optional<ReportDTO.Report> findByIdAndTargetType(@Param("id") Long id,
-            @Param("target_type") String target_type);
+        @Query(value = "SELECT DISTINCT p.resolved AS resolved, p.created_at AS reportedAt, p.id AS id, p.target_id AS targetId, p.user_id AS reportedBy, p.reason AS reason, p.target_type AS targetType, u.user_name FROM reports p INNER JOIN users u ON p.user_id=u.id", nativeQuery = true)
+        public Page<AllReports> getNewRports(Pageable pageable);
 
     public void deleteByTargetIdAndTargetType(Long id, ReportTargetType targetType);
 
-    @Query(value = "SELECT DISTINCT p.*, u.user_name FROM reports p INNER JOIN users u ON p.target_type=:target_type", nativeQuery = true)
+    @Query(value = "SELECT DISTINCT p.resolved AS resolved, p.created_at AS reportedAt, p.id AS id, p.target_id AS targetId, p.user_id AS reportedBy, p.reason AS reason, p.target_type AS targetType, u.user_name FROM reports p INNER JOIN users u ON p.target_type=:target_type", nativeQuery = true)
     public List<AllReports> findByTargetType(@Param("target_type") String target_type, Pageable pageable);
 
-    @Query(value = "SELECT DISTINCT p.*, u.user_name FROM reports p INNER JOIN users u ON p.user_id=u.id", nativeQuery = true)
+    @Query(value = "SELECT DISTINCT p.resolved AS resolved, p.created_at AS reportedAt, p.id AS id, p.target_id AS targetId, p.user_id AS reportedBy, p.reason AS reason, p.target_type AS targetType, u.user_name FROM reports p INNER JOIN users u ON p.user_id=u.id", nativeQuery = true)
     public Streamable<AllReports> findAllReports();
 
 }
